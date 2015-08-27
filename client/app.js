63503241d254ac3e339b5b6f1b2353302f2ce952
azellsworth <azellsworth@gmail.com>
2015-08-27 15:53:17 -0700

    (feat) mvp functionality of sockets

--- client/app.js
+++ client/app.js
@@ -3,9 +3,11 @@ var socket = io.connect('http://localhost:9090');
 // when button is clicked on client side, emit button push to other users
 $('.push-button').on('click', function(event){
   console.log('CLICKED BUTTON');
-  socket.emit('button clicked');
+  socket.emit('button clicked', {meatloaf: "unexpected"});
 })
 
 // when button is clicked by other users, briefly change color of top square
-socket.on 
+socket.on('friend click', function(data){
+  console.log('Someone else clicked a button!');
+});
 
--- server/server.js
+++ server/server.js
@@ -5,9 +5,10 @@ var server = http.Server(app);
 var io = require('socket.io')(server)
 
 io.on('connection', function(socket) {
-  console.log("Connection happening");
+  console.log("Connection happening", socket.id);
   socket.on('button clicked', function(data){
-    console.log("Someone pressed a button!");
+    console.log("Someone pressed a button! ", data);
+    io.emit("friend click");
   })
 });
 
