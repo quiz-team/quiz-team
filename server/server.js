07e8c53571dee62e2f8f2b8d17d684202df2d898
Kenneth Ho <hokennethk@users.noreply.github.com>
2015-08-27 16:34:36 -0700

    (feat) styled buttons and implement mousedown/mouseup change

--- server/server.js
+++ server/server.js
@@ -3,15 +3,25 @@
 var app = require('./express.js');
 var http = require('http');
 var server = http.Server(app);
-var io = require('socket.io')(server)
+var io = require('socket.io')(server);
+
+var numClicked = 0;
 
 //Event for dealing with incoming socket connection.
 io.on('connection', function(socket) {
   console.log("Connection happening", socket.id);
+
   socket.on('button clicked', function(data){
-    console.log("Someone pressed a button! ", data);
+    numClicked += 1;
     io.emit("friend click");
-  })
+  });
+
+  socket.on('button unclicked', function(data){
+    numClicked -= 1;
+    if (numClicked === 0) {
+      io.emit("friend unclick");
+    }
+  });
 });
 
 server.listen('9090');
