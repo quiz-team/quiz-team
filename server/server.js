2c0c12ec7501baf140dea40796e41ad87ac178bd
atcastle <atcastle@gmail.com>
2015-08-27 15:23:28 -0700

    (feat) Add socket support to server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server)
 
+io.on('connection', function(socket) {
+  console.log("Connection happening");
+});
+
+
+//Routing stuff. TODO: Move to different file
 app.get('/', function(req, res) {
   res.redirect('/index.html');
 });
 
 app.use(express.static('client'));
+//End routing stuff
 
-app.listen('9090');
+server.listen('9090');
